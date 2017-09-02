b758de7a0ce3428e1dc84bb2e9dc84fa0a225bad
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-02 03:33:45 -0400

    Parse a Script

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -12,7 +12,9 @@ int main(int argc, char *argv[], char *envp[]) {
   if(argc > 0) {
     executeScript(argv[1]);
   }
-  loopTerminal();
+  else {
+    loopTerminal();
+  }
   return 0;
 }
 
@@ -67,8 +69,56 @@ int parseLine(char *line, char args[][TOKENSIZE]) {
   }
   return tokenIndex;
 }
-
+/*
+* Executes the script whose name is provided in filename
+*/
 int executeScript(char *fileName) {
-  //char fileContent[][BUFFERSIZE];
+  int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0, pos = 0, commentFlag = 0;
+  int numTokens = MAXNUMTOKENS;
+  char line[buffSize];
+  char args[numTokens][TOKENSIZE];
+  char *checkStart = "#!sbush", ch;
+  FILE *fp = fopen(fileName,"r");
+  puts("sbush> ");
+  if(!fp) return 0;
+  /* Check if script starts with #!sbush*/
+  for(i = 0; i < 7; i++) {
+    ch = fgetc(fp);
+    if(ch != checkStart[i]) return 0;
+  }
+  /*Skip the first Line*/
+  while((ch = getc(fp)) != '\n');
+  while ((ch = getc(fp)) != EOF) {
+    if(ch != '\n') {  
+      if (ch == '#')
+      {
+        line[pos] = '\0';
+        commentFlag = 1;
+      }
+      else {
+        if(commentFlag == 1) {
+          line[pos] = '\0';
+        }
+        else {
+          line[pos] = ch;
+          pos++;
+        }
+      }
+    }
+    else {
+      line[pos] = '\0';
+      tokensParsed = parseLine(line, args);
+      pos = 0;
+      commentFlag = 0;
+      for(i = 0; i < tokensParsed; i++) {
+        printf("%s ", args[i]);
+      }
+    }
+  }
+  line[pos] = '\0';
+  tokensParsed = parseLine(line, args);
+  for(i = 0; i < tokensParsed; i++) {
+    printf("%s ", args[i]);
+  }
   return 0;
 }
