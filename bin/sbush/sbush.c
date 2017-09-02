b11153716e7fa1858118fe5cb7cff2202088658f
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-02 02:01:04 -0400

    Variable rename and comments

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -6,17 +6,22 @@
 int loopTerminal();
 int readLine(char*);
 int parseLine(char*, char [][TOKENSIZE]);
+int executeScript(char *);
 
 int main(int argc, char *argv[], char *envp[]) {
-  puts("sbush> ");
+  if(argc > 0) {
+    executeScript(argv[1]);
+  }
   loopTerminal();
   return 0;
 }
+
 int loopTerminal() {
   int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0;
   int numTokens = MAXNUMTOKENS;
   char line[buffSize];
   char args[numTokens][TOKENSIZE];
+  puts("sbush> ");
   readLine(line);
   tokensParsed = parseLine(line, args);
   for(i = 0; i < tokensParsed; i++) {
@@ -24,6 +29,9 @@ int loopTerminal() {
   }
   return 0;
 }
+/*
+* Reads a line from console
+*/
 int readLine(char *line) {
   unsigned char c;
   int pos = 0;
@@ -40,20 +48,27 @@ int readLine(char *line) {
   }
   return 0;
 }
+/* 
+* Parses a line and returns the number of tokens generated
+*/
 int parseLine(char *line, char args[][TOKENSIZE]) {
-  int i = 0, j = 0, k = 0;
-  while(line[i] != '\0') {
-        k = 0;
-    	while((line[i] != ' ' || line[i] == '\t') && line[i] != '\0') {
-          args[j][k] = line[i];
-          i++;k++;
+  int charPos = 0, tokenIndex = 0, charTokenPos = 0;
+  while(line[charPos] != '\0') {
+        charTokenPos = 0;
+        while((line[charPos] != ' ' || line[charPos] == '\t') && line[charPos] != '\0') {
+          args[tokenIndex][charTokenPos] = line[charPos];
+          charPos++;charTokenPos++;
         }
-        if(line[i] == ' ' || line[i] == '\t') {
-		i++;
+        if(line[charPos] == ' ' || line[charPos] == '\t') {
+		charPos++;
         }
-	args[j][k] = '\0';
-        j++;
+	args[tokenIndex][charTokenPos] = '\0';
+        tokenIndex++;
   }
-  return j;
+  return tokenIndex;
 }
 
+int executeScript(char *fileName) {
+  //char fileContent[][BUFFERSIZE];
+  return 0;
+}
