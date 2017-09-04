fe7bd687e5f34ba9553a0f1e61d8767a71fa6587
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-03 20:21:40 -0400

    Code to execute processes

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,19 +1,32 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <unistd.h>
 #define BUFFERSIZE 10000
 #define MAXNUMTOKENS 100
 #define TOKENSIZE 100
-
+int initArr(char [][TOKENSIZE], int, int);
 int loopTerminal();
 int readLine(char*);
 int parseLine(char*, char [][TOKENSIZE]);
 int executeScript(char *);
+int executeCommand(char [][TOKENSIZE]);
 
 int main(int argc, char *argv[], char *envp[]) {
-  if(argc > 0) {
+  if(argc > 1) {
     executeScript(argv[1]);
   }
   else {
     loopTerminal();
+  }
+  return 0;
+}
+
+int initArr(char args[][TOKENSIZE], int x, int y) {
+  int i = 0, j = 0;
+  for(i = 0; i < x; i++) {
+    for(j = 0; j < y; j++) {
+      args[i][j] = '\0';
+    }
   }
   return 0;
 }
@@ -23,11 +36,13 @@ int loopTerminal() {
   int numTokens = MAXNUMTOKENS;
   char line[buffSize];
   char args[numTokens][TOKENSIZE];
+  initArr(args, numTokens, TOKENSIZE);
   puts("sbush> ");
   readLine(line);
   tokensParsed = parseLine(line, args);
+  executeCommand(args);
   for(i = 0; i < tokensParsed; i++) {
-    printf("%s ", args[i]);
+    printf("%s\n", args[i]);
   }
   return 0;
 }
@@ -57,12 +72,13 @@ int parseLine(char *line, char args[][TOKENSIZE]) {
   int charPos = 0, tokenIndex = 0, charTokenPos = 0;
   while(line[charPos] != '\0') {
         charTokenPos = 0;
-        while((line[charPos] != ' ' || line[charPos] == '\t') && line[charPos] != '\0') {
+        while((line[charPos] != ' ' && line[charPos] != '\t') && line[charPos] != '\0') {
           args[tokenIndex][charTokenPos] = line[charPos];
           charPos++;charTokenPos++;
         }
         if(line[charPos] == ' ' || line[charPos] == '\t') {
-		charPos++;
+                charPos++;
+                while(line[charPos] == ' ') charPos++;
         }
 	args[tokenIndex][charTokenPos] = '\0';
         tokenIndex++;
@@ -119,6 +135,24 @@ int executeScript(char *fileName) {
   tokensParsed = parseLine(line, args);
   for(i = 0; i < tokensParsed; i++) {
     printf("%s ", args[i]);
+  }
+  return 0;
+}
+int executeCommand(char args[][TOKENSIZE]) {
+  pid_t pid;
+  char *argv[] = {args[0],(char*)0};
+  //char *argv[] =  {"ls", (char*)0};
+  pid = fork();
+  if(pid < 0) {
+    return 1;
+  }
+  else if (pid == 0) {
+    if (execvp(argv[0], argv) == -1) {
+      perror("sbush");
+    }
+  }
+  else {
+      //wait(NULL);
   }
   return 0;
 }
