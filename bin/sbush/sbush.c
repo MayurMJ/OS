af75c55796baf092def9931b87c83bdb1bffe66b
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-03 23:39:24 -0400

    Added support for pipe parsing

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -7,9 +7,9 @@
 
 int loopTerminal();
 int readLine(char*);
-int parseLine(char*, char [][TOKENSIZE]);
+int parseLine(char*, char [][TOKENSIZE], int*);
 int executeScript(char *);
-int executeCommand(char [][TOKENSIZE], int);
+int executeCommand(char [][TOKENSIZE], int, int);
 
 int main(int argc, char *argv[], char *envp[]) {
   if(argc > 1) {
@@ -22,15 +22,15 @@ int main(int argc, char *argv[], char *envp[]) {
 }
 
 int loopTerminal() {
-  int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0, status = 0;
+  int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0, status = 0, pipeCount = 0;
   int numTokens = MAXNUMTOKENS;
   char line[buffSize];
   char args[numTokens][TOKENSIZE];
   while(status == 0) {
     puts("sbush> ");
     readLine(line);
-    tokensParsed = parseLine(line, args);
-    status = executeCommand(args, tokensParsed);
+    tokensParsed = parseLine(line, args, &pipeCount);
+    status = executeCommand(args, tokensParsed, pipeCount);
   }
   for(i = 0; i < tokensParsed; i++) {
     printf("%s\n", args[i]);
@@ -59,12 +59,13 @@ int readLine(char *line) {
 /* 
 * Parses a line and returns the number of tokens generated
 */
-int parseLine(char *line, char args[][TOKENSIZE]) {
+int parseLine(char *line, char args[][TOKENSIZE], int *pipeCount) {
   int charPos = 0, tokenIndex = 0, charTokenPos = 0;
   while(line[charPos] != '\0') {
         charTokenPos = 0;
         while((line[charPos] != ' ' && line[charPos] != '\t') && line[charPos] != '\0') {
           args[tokenIndex][charTokenPos] = line[charPos];
+          if(line[charPos] == '|') *pipeCount = *pipeCount + 1;
           charPos++;charTokenPos++;
         }
         if(line[charPos] == ' ' || line[charPos] == '\t') {
@@ -80,7 +81,7 @@ int parseLine(char *line, char args[][TOKENSIZE]) {
 * Executes the script whose name is provided in filename
 */
 int executeScript(char *fileName) {
-  int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0, pos = 0, commentFlag = 0;
+  int buffSize = BUFFERSIZE, i = 0, tokensParsed = 0, pos = 0, commentFlag = 0, parseCount = 0;
   int numTokens = MAXNUMTOKENS;
   char line[buffSize];
   char args[numTokens][TOKENSIZE];
@@ -114,7 +115,7 @@ int executeScript(char *fileName) {
     }
     else {
       line[pos] = '\0';
-      tokensParsed = parseLine(line, args);
+      tokensParsed = parseLine(line, args, &parseCount);
       pos = 0;
       commentFlag = 0;
       for(i = 0; i < tokensParsed; i++) {
@@ -123,33 +124,41 @@ int executeScript(char *fileName) {
     }
   }
   line[pos] = '\0';
-  tokensParsed = parseLine(line, args);
+  tokensParsed = parseLine(line, args, &parseCount);
   for(i = 0; i < tokensParsed; i++) {
     printf("%s ", args[i]);
   }
   return 0;
 }
-int executeCommand(char args[][TOKENSIZE], int tokenCount) {
+int executeCommand(char args[][TOKENSIZE], int tokenCount, int pipeCount) {
   pid_t pid;
-  int i;
-  char *argv[tokenCount];// = {args[0],(char*)0};
+  int i = 0, j = 0, k = 0;
+  typedef char *commands[tokenCount];
+  commands c[pipeCount+1];
   for(i = 0; i < tokenCount; i++) {
-    argv[i] = args[i];
-  }
-  argv[i] = (char*)0; 
-  //char *argv[] =  {"ls", (char*)0};
-  pid = fork();
-  if(pid < 0) {
-    return 1;
-  }
-  else if (pid == 0) {
-    if (execvp(argv[0], argv) == -1) {
-      perror("sbush");
-      return -1;
+    j = 0;
+    while(args[i][0] != '|' && i < tokenCount) {
+      c[k][j] = args[i];
+      j++;i++; 
     }
+    c[k][j] = (char*)0; 
+    k++;
   }
-  else {
+  //char *argv[] =  {"ls", (char*)0};
+  for(i = 0; i < pipeCount + 1; i++) {
+    pid = fork();
+    if(pid < 0) {
+      return 1;
+    }
+    else if (pid == 0) {
+      if (execvp(c[i][0], c[i]) == -1) {
+        perror("sbush");
+        return -1;
+      }
+    }
+    else {
       //wait(NULL);
+    }
   }
   return 0;
 }
