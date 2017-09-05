582aeeed23a53b0d96c311745e0b2b4f7796a990
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-05 16:05:44 -0400

    Implemented main and write SysCall

--- include/unistd.h
+++ include/unistd.h
@@ -14,17 +14,17 @@ char *getcwd(char *buf, size_t size);
 
 pid_t fork();
 int execvpe(const char *file, char *const argv[], char *const envp[]);
-pid_t wait(int *status)
+pid_t wait(int *status);
 int waitpid(int pid, int *status);
 
-unsigned int sleep(unsigned int seconds);
+//unsigned int sleep(unsigned int seconds);
 
 pid_t getpid(void);
 pid_t getppid(void);
 
 // OPTIONAL: implement for ``on-disk r/w file system (+10 pts)''
 off_t lseek(int fd, off_t offset, int whence);
-int mkdir(const char *pathname, mode_t mode);
+//int mkdir(const char *pathname, mode_t mode);
 
 // OPTIONAL: implement for ``signals and pipes (+10 pts)''
 int pipe(int pipefd[2]);
--- libc/puts.c
+++ libc/puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int puts(const char *s)
 {
-  for( ; *s; ++s) if (putchar(*s) != *s) return EOF;
-  return (putchar('\n') == '\n') ? 0 : EOF;
+  //for( ; *s; ++s) if (putchar(*s) != *s) return EOF;
+  //return (putchar('\n') == '\n') ? 0 : EOF;
+  int len = 0;
+  while(s[len] != '\0') {
+    len++;
+  }
+  return write(1, s, len);  
 }
