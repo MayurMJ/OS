60696cb4160a4f57dd0bd67cc45c150b472cace2
Raghav Dogra <raghavdogra@gmail.com>
2017-09-06 01:39:44 -0400

    add syscall0 and syscall4
    Signed-off-by: Raghav Dogra <raghavdogra@gmail.com>

--- include/syscall.h
+++ include/syscall.h
@@ -3,6 +3,13 @@
 
 #include <sys/defs.h>
 
+static inline long syscall4(long syscall, long arg1, long arg2, long arg3, long arg4) {
+long ret;
+__asm volatile ("syscall" : "=a" (ret) : "a" (syscall), "b" (arg1), "c" (arg2),
+ "d" (arg3), "e" (arg4) : "memory");
+return ret;
+}
+
 static inline long syscall3(long syscall, long arg1, long arg2, long arg3) {
 long ret;
 __asm volatile ("syscall" : "=a" (ret) : "a" (syscall), "b" (arg1), "c" (arg2),
@@ -19,6 +26,12 @@ return ret;
 static inline long syscall1(long syscall, long arg1) {
 long ret;
 __asm volatile ("syscall" : "=a" (ret) : "a" (syscall), "b" (arg1) : "memory");
+return ret;
+}
+
+static inline long syscall0(long syscall, long arg1) {
+long ret;
+__asm volatile ("syscall" : "=a" (ret) : "a" (syscall) : "memory");
 return ret;
 }
 
