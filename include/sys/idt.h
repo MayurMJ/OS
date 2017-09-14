258a1fa7168863fe3ebf272b601f3aa41f168c27
Mayur jadhav <mayurjadhav72@gmail.com>
2017-09-13 20:22:45 -0400

    Constructor for IDT

--- include/sys/idt.h
+++ include/sys/idt.h
@@ -2,17 +2,19 @@
 #define IDT_H_
 #include <sys/defs.h>
 
-typedef struct IDTDescr {
-   uint16_t offset_1; // offset bits 0..15
-   uint16_t selector;// = 8; // a code segment selector in GDT or LDT
-   uint8_t ist;       // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-   uint8_t type_attr;// = 14; // type and attributes
-   uint16_t offset_2; // offset bits 16..31
-   uint32_t offset_3; // offset bits 32..63
-   uint32_t zero;// = 0;     // reserved
-}idt_entry;
-idt_entry idt[256];
+
+
+struct IDTDescr {
+   uint16_t id_offset_1 :16; // offset bits 0..15
+   uint16_t id_selector :16;// = 8; // a code segment selector in GDT or LDT
+   uint8_t id_ist :3;       // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+   uint8_t id_type_attr : 8;// = 14; // type and attributes
+   uint16_t id_offset_2 : 16; // offset bits 16..31
+   uint32_t id_offset_3: 32; // offset bits 32..63
+   uint32_t id_zero :32;// = 0;     // reserved
+};
+struct IDTDescr idt[256];
 
 void init_idt();
-
+void set_idt(struct IDTDescr *idt_entry, uint16_t offset_1, uint16_t selector, uint8_t ist, uint16_t type_attr, uint16_t offset_2, uint32_t offset_3);
 #endif
--- sys/idt.c
+++ sys/idt.c
@@ -6,4 +6,13 @@ void init_idt() {
 
              		);
 }
+void set_idt(struct IDTDescr *idt_entry, uint16_t offset_1, uint16_t selector, uint8_t ist, uint16_t type_attr, uint16_t offset_2, uint32_t offset_3){
+	idt_entry->id_offset_1 	= offset_1;
+	idt_entry->id_selector 	= selector;
+	idt_entry->id_ist 	= ist;
+	idt_entry->id_type_attr = type_attr;
+	idt_entry->id_offset_2 	= offset_2;
+	idt_entry->id_offset_3 	= offset_3;
+	idt_entry->id_zero 	= 0;
+}
 
