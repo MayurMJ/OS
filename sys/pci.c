d90e63893f6ae4877d6cfbf780e7210b9004bf4c
Gargi Saha <gsaha@cs.stonybrook.edu>
2017-09-25 18:09:26 -0400

    small busg fixed

--- sys/pci.c
+++ sys/pci.c
@@ -184,93 +184,6 @@ int find_cmdslot(hba_port_t *port)
 	kprintf("Cannot find free command list entry\n");
 	return -1;
 }
-int write_port(hba_port_t *port, uint32_t startl, uint32_t starth, uint32_t count, uint16_t *buf)
-{
-	port->is_rwc = (uint32_t)-1;		// Clear pending interrupt bits
-	int spin = 0; // Spin lock timeout counter
-	int slot = find_cmdslot(port), i = 0;
-	if (slot == -1)
-		return FALSE;
- 
-	hba_cmd_header_t *cmdheader = (hba_cmd_header_t*)port->clb;
-	cmdheader += slot;
-	cmdheader->cfl = sizeof(fis_reg_d2h_t)/sizeof(uint32_t);	// Command FIS size
-	cmdheader->w = 1;		// Read from device
-	cmdheader->prdtl = (uint16_t)((count-1)>>4) + 1;	// PRDT entries count
- 
-	hba_cmd_tbl_t *cmdtbl = (hba_cmd_tbl_t*)(cmdheader->ctba);
-	memset(cmdtbl, 0, sizeof(hba_cmd_tbl_t) +
- 		(cmdheader->prdtl-1)*sizeof(hba_prdt_entry_t));
-	// 8K bytes (16 sectors) per PRDT
-	for (i=0; i<cmdheader->prdtl; i++)
-	{
-		memset(buf,i,4096);
-		cmdtbl->prdt_entry[i].dba = (uint64_t)buf;
-		cmdtbl->prdt_entry[i].dbc = 8*1024;	// 8K bytes
-		cmdtbl->prdt_entry[i].i = 1;
-		buf += 4*1024;	// 4K words
-		count -= 16;	// 16 sectors
-	}
-	// Last entry
-	//cmdtbl->prdt_entry[i].dba = (uint64_t)buf;
-	//cmdtbl->prdt_entry[i].dbc = count<<9;	// 512 bytes per sector
-	//cmdtbl->prdt_entry[i].i = 1;
- 
-	// Setup command
-	fis_reg_h2d_t *cmdfis = (fis_reg_h2d_t*)(&cmdtbl->cfis);
- 
-	cmdfis->fis_type = FIS_TYPE_REG_H2D;
-	cmdfis->c = 1;	// Command
-	cmdfis->command = ATA_CMD_WRITE_DMA_EX;
- 
-	cmdfis->lba0 = (uint8_t)startl;
-	cmdfis->lba1 = (uint8_t)(startl>>8);
-	cmdfis->lba2 = (uint8_t)(startl>>16);
-	cmdfis->device = 1<<6;	// LBA mode
- 
-	cmdfis->lba3 = (uint8_t)(startl>>24);
-	cmdfis->lba4 = (uint8_t)starth;
-	cmdfis->lba5 = (uint8_t)(starth>>8);
- 
-	cmdfis->count = count;
-	//cmdfis->counth = 0;
- 
-	// The below loop waits until the port is no longer busy before issuing a new command
-	while ((port->tfd & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000)
-	{
-		spin++;
-	}
-	if (spin == 1000000)
-	{
-		kprintf("Port is hung\n");
-		return FALSE;
-	}
- 
-	port->ci = 1<<slot;	// Issue command
- 
-	// Wait for completion
-	while (1)
-	{
-		// In some longer duration reads, it may be helpful to spin on the DPS bit 
-		// in the PxIS port field as well (1 << 5)
-		if ((port->ci & (1<<slot)) == 0) 
-			break;
-		if (port->is_rwc & HBA_PxIS_TFES)	// Task file error
-		{
-			kprintf("Read disk error\n");
-			return FALSE;
-		}
-	}
- 
-	// Check again
-	if (port->is_rwc & HBA_PxIS_TFES)
-	{
-		kprintf("Read disk error\n");
-		return FALSE;
-	}
- 
-	return TRUE;
-}
 
 #define	AHCI_BASE	0x100000
  
@@ -361,14 +274,14 @@ int read(hba_port_t *port, uint32_t startl, uint32_t starth, uint32_t count, uin
 	int i = 0;
 	for (i=0; i<cmdheader->prdtl-1; i++)
 	{
-		cmdtbl->prdt_entry[i].dba = (uint64_t)buf;
+		cmdtbl->prdt_entry[i].dba = (uint64_t)(buf & 0xFFFFFFFFFFFFFFFF);;
 		cmdtbl->prdt_entry[i].dbc = 8*1024 - 1;	// 8K bytes
 		cmdtbl->prdt_entry[i].i = 1;
 		buf += 4*1024;	// 4K words
 		count -= 16;	// 16 sectors
 	}
 	// Last entry
-	cmdtbl->prdt_entry[i].dba = (uint64_t)buf;
+	cmdtbl->prdt_entry[i].dba = (uint64_t)(buf & 0xFFFFFFFFFFFFFFFF);
 	cmdtbl->prdt_entry[i].dbc = count<<9;	// 512 bytes per sector
 	cmdtbl->prdt_entry[i].i = 1;
  
@@ -452,14 +365,14 @@ int write(hba_port_t *port, uint32_t startl, uint32_t starth, uint32_t count, ui
 	for (i=0; i<cmdheader->prdtl-1; i++)
 	{
 		//memset(buf, 1, 4096);
-		cmdtbl->prdt_entry[i].dba = buf ;//& (0xFFFFFFFF);
+		cmdtbl->prdt_entry[i].dba = (uint64_t)(buf & 0xFFFFFFFFFFFFFFFF) ;//& (0xFFFFFFFF);
 		cmdtbl->prdt_entry[i].dbc = 8*1024 - 1;	// 8K bytes
 		cmdtbl->prdt_entry[i].i = 0;
 		buf += 4*1024;	// 4K words
 		count -= 16;	// 16 sectors
 	}
 	// Last entry
-	cmdtbl->prdt_entry[i].dba = (uint64_t)buf ;//&(0xFFFFFFFF);
+	cmdtbl->prdt_entry[i].dba = (uint64_t)(buf & 0xFFFFFFFFFFFFFFFF);//&(0xFFFFFFFF);
 	cmdtbl->prdt_entry[i].dbc = count<<9;	// 512 bytes per sector
 	cmdtbl->prdt_entry[i].i = 0;
 	 
