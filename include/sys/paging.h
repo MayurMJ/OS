07aff8a41fa30f6364ecc1959927bb0b90c218fd
Gargi Saha <gsaha@cs.stonybrook.edu>
2017-10-30 19:06:43 -0400

    paging.h included

--- include/sys/paging.h
+++ include/sys/paging.h
@@ -5,8 +5,10 @@ typedef struct {
 	uint64_t last_addr;
 } smap_copy_t;
 
-typedef struct {
+typedef struct pg_desc {
 	uint8_t is_avail;
+	struct pg_desc *prev;
+	struct pg_desc *next; 
 }__attribute__((__packed__)) pg_desc_t;
 
 pg_desc_t *free_list;
