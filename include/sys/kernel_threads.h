b6221629fa560d18032aed42bf97d86040a43bc5
Gargi Saha <gsaha@cs.stonybrook.edu>
2017-11-04 18:16:23 -0400

    kernel threads.h file

---
+++ include/sys/kernel_threads.h
@@ -0,0 +1,16 @@
+#ifndef _KERN_THD
+#define _KERN_THD
+
+typedef struct {
+    uint64_t rax, rbx, rcx, rdx, rsi, rdi, rsp, rbp, rip, rflags, cr3;
+} Registers;
+
+
+typedef struct TASK {
+   uint64_t *kstack;
+   enum { RUNNING, SLEEPING, ZOMBIE } state;
+   Registers reg;
+   struct TASK *next;
+} Task ;
+
+#endif
