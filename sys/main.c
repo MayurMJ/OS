3cf66e439b128b8fe68bbbb3b2dd66f7148bed49
Raghav Dogra <raghavdogra@gmail.com>
2017-11-13 22:11:53 -0500

    fix PML4

--- sys/main.c
+++ sys/main.c
@@ -128,13 +128,13 @@ void start(uint32_t *modulep, void *physbase, void *physfree)
   uint64_t free_list_end = setup_memory(physbase, physfree, smap_copy, smap_copy_index);
   /*remap the 640K-1M region with direct one to one mapping from virtual to physical*/
   init_self_referencing(free_list_end);
-  map_memory_range(0xa0000, 0x100000, 0);
   int i = 0;
   for(i=0;i<510;i++) {
-    uint64_t *PTE = (uint64_t *)(uint64_t) free_list_end + (uint64_t)(4096*(i+1));
+    uint64_t PTE = (uint64_t) free_list_end + (uint64_t)(4096*(i+1));
     PML4[i] = (uint64_t) PTE;
     PML4[i] = PML4[i] | 3; 
   }
+  map_memory_range(0xa0000, 0x100000, 0);
 
   map_memory_range((uint64_t)physbase, free_list_end + (520*4096), 1);
 
