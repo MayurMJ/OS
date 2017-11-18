94a6fa07c0f591258767b501f0f48869faddae81
Mayur jadhav <mayurjadhav72@gmail.com>
2017-11-18 15:19:48 -0500

    Bug Fix in get_free_pages

--- sys/paging.c
+++ sys/paging.c
@@ -147,8 +147,8 @@ void init_self_referencing(uint64_t free_list_end, uint64_t index) {
   uint64_t temp = (uint64_t)free_list_end+(uint64_t)12288;
   //PTE1 = (uint64_t *)((uint64_t)free_list_end+(uint64_t)12288);
   //uint64_t * PTE2 = (uint64_t *)((uint64_t)free_list_end+(uint64_t)16384);
-  PML4[511] = ((uint64_t)PDTP) | 7;
-  PDTP[510] = ((uint64_t)PDE) | 7; 
+  PML4[511] = ((uint64_t)PDTP) |(uint64_t) 7;
+  PDTP[510] = ((uint64_t)PDE) | (uint64_t) 7; 
   for(int i = 0; i < 510; i++) {
     PML4[i] = 0;
   }
@@ -172,7 +172,7 @@ void map_memory_range(uint64_t start, uint64_t end, uint64_t map_index) {
     PTE[i] = 0;
   }
   for(uint64_t x = (uint64_t)start; x < (uint64_t) end; x += 4096) {
-    PTE[ind] = x | 7;
+    PTE[ind] = x | (uint64_t) 7;
     ind++;
   }
   for(int i = ind; i < 512; i++) {
@@ -197,7 +197,7 @@ uint64_t get_free_page(uint64_t flags) {
   uint64_t *PDPTE;
   if(PML4[PMLframe] == 0) {
     PDPTE = (uint64_t *)get_physical_free_page();
-    PML4[PMLframe] = (uint64_t) PDPTE | 7;
+    PML4[PMLframe] = (uint64_t) PDPTE | (uint64_t) 7;
     x = (uint64_t)0xffffffff80000000 + (uint64_t) PML4[PMLframe];
     x = x & 0xfffffffffffff000;
     PDPTE = (uint64_t *) x;
@@ -216,7 +216,7 @@ uint64_t get_free_page(uint64_t flags) {
   uint64_t *PDE;
   if(PDPTE[PDPTEindex] == 0) {
     PDE = (uint64_t *)get_physical_free_page();
-    PDPTE[PDPTEindex] = (uint64_t) PDE | 7;
+    PDPTE[PDPTEindex] = (uint64_t) PDE | (uint64_t) 7;
     x = (uint64_t)0xffffffff80000000 + (uint64_t) PDPTE[PDPTEindex];
     x = x & 0xfffffffffffff000; 
     PDE = (uint64_t *) x;
@@ -235,7 +235,7 @@ uint64_t get_free_page(uint64_t flags) {
   uint64_t *PTE;
   if(PDE[PDEindex] == 0) {
     PTE = (uint64_t *)get_physical_free_page();
-    PDE[PDEindex] = (uint64_t) PTE | 7;
+    PDE[PDEindex] = (uint64_t) PTE | (uint64_t) 7;
     x = (uint64_t)0xffffffff80000000 + (uint64_t) PDE[PDEindex];
     x = x & 0xfffffffffffff000; 
     PTE = (uint64_t *) x;
@@ -247,7 +247,7 @@ uint64_t get_free_page(uint64_t flags) {
   else {
     x = (uint64_t)0xffffffff80000000 + (uint64_t) PDE[PDEindex];
     x = x & 0xfffffffffffff000; 
-    PTE = (uint64_t *) (PDE[PDEindex] + (uint64_t)0xffffffff80000000);
+    PTE = (uint64_t *) x;
   }
 
 //PTE
