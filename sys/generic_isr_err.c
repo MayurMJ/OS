74842c967e8e9eb2f900baa28110a816ec3f7442
Raghav Dogra <raghavdogra@gmail.com>
2017-11-22 02:19:17 -0500

    print the address which caused page fault in page fault handler

--- sys/generic_isr_err.c
+++ sys/generic_isr_err.c
@@ -1,5 +1,5 @@
 #include <sys/kprintf.h>
-//#include <sys/defs.h>
+#include <sys/defs.h>
 //#include <sys/ahci.h>
 void generic_irqhandler_err8(void)
 {
@@ -34,5 +34,15 @@ void generic_irqhandler_err13(void)
 void generic_irqhandler_err14(void)
 {
     kprintf("Generic interrupt occured with error code 14\n");
+    uint64_t cr2val;
+    uint64_t cr3val;
+    __asm__ __volatile__("movq %%cr2, %0\n\t"
+                             :"=a"(cr2val)); 
+    __asm__ __volatile__("movq %%cr3, %0\n\t"
+                             :"=a"(cr3val)); 
+    kprintf("page fault while accessing address %x", cr2val);
+    kprintf("cr3 %x", cr3val);
+//    put_page_mapping(3, cr2val, cr3val);
+    while(1);
 
 }
