0de7a96f717e20d5825d799d23965209f1be9227
Mayur jadhav <mayurjadhav72@gmail.com>
2017-11-24 04:43:46 -0500

    Added FIles for XOW

---
+++ include/sys/copy_tables.h
@@ -0,0 +1,7 @@
+#ifndef _COPY_TABLES_H
+#define _COPY_TABLES_H
+
+uint64_t create_table();
+uint64_t copy_on_write();
+
+#endif
---
+++ sys/copy_tables.c
@@ -0,0 +1,41 @@
+#include <sys/paging.h>
+uint64_t create_table() {
+	uint64_t *newPML4 = (uint64_t*) get_physical_free_page();
+	uint64_t *temp = (uint64_t *)((uint64_t) newPML4 + (uint64_t)0xffffffff80000000 );
+ 	for(int i = 0; i < 512; i++) {
+		temp[i] = PML4[i];
+	}
+	return (uint64_t) newPML4;
+}
+
+uint64_t copy_on_write() {
+	uint64_t oldcr3;
+	__asm__ __volatile__("movq %%cr3, %0\n\t"
+			     :"=a"(oldcr3));
+	uint64_t newcr3 = create_table();
+	uint64_t *PML4_old = (uint64_t *)((uint64_t) oldcr3 + (uint64_t)0xffffffff80000000 );
+	uint64_t *PML4_new = (uint64_t *)((uint64_t) newcr3 + (uint64_t)0xffffffff80000000 );
+	for(int i = 0; i < 512; i++) {
+		PML4_new[i] = 0;
+		if(PML4_old[i] != 0 && i != 512) {
+			PML4_new[i] = get_physical_free_page();
+			uint64_t *PDTP_old = (uint64_t *)((uint64_t) PML4_old[i] + (uint64_t)0xffffffff80000000 );
+			uint64_t *PDTP_new = (uint64_t *)((uint64_t) PML4_new[i] + (uint64_t)0xffffffff80000000 );
+			for(int j = 0; j < 512; j++) {
+				PDTP_new[j] = 0;
+				if(PDTP_old[j] !=0 && i != 512 && j !=511 ) {
+					PDTP_new[j] = get_physical_free_page();			
+					uint64_t *PDE_old = (uint64_t *)((uint64_t) PDTP_old[j] + (uint64_t)0xffffffff80000000 );
+					uint64_t *PDE_new = (uint64_t *)((uint64_t) PDTP_new[j] + (uint64_t)0xffffffff80000000 );
+					for(int k = 0; k < 512; k++) {
+						PDE_new[k] = PDE_old[k];
+						PDE_new[k] = (PDE_new[k] >> 12) << 12;
+						PDE_new[k] |= (uint64_t) 0x805; 
+					}
+				}
+			}
+			
+		}
+	}
+	return newcr3;	
+}
