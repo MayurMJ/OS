a1c372c948e66a550d7c26d5d83671cdc708d380
Mayur jadhav <mayurjadhav72@gmail.com>
2017-11-24 01:07:29 -0500

    Fixed indentation and added coments in elf parsing code

--- sys/elf.c
+++ sys/elf.c
@@ -64,95 +64,96 @@ uint64_t stoi(char *s) // the message and then the line #
 }*/
 Task *loadElf(char *fileName) { 
   
-  struct posix_header_ustar * header = (struct posix_header_ustar *)&_binary_tarfs_start;
-  kprintf("size of header %d",sizeof(struct posix_header_ustar));
-  while(header<(struct posix_header_ustar *)&_binary_tarfs_end) {
-    uint64_t size = octalToDecimal(stoi(header->size));
-    if (size == 0)
-      header++;
-    else {
-      Elf64_Ehdr *elfhdr = (Elf64_Ehdr *) (header+1);
-      if((elfhdr->e_ident[0]==0x7f)&&(elfhdr->e_ident[1]==0x45)&&
-        (elfhdr->e_ident[2]==0x4c)&&(elfhdr->e_ident[3]==0x46)&& (!strcmp(header->name,fileName))) {
+	struct posix_header_ustar * header = (struct posix_header_ustar *)&_binary_tarfs_start;
+	kprintf("size of header %d",sizeof(struct posix_header_ustar));
+	while(header<(struct posix_header_ustar *)&_binary_tarfs_end) {
+		uint64_t size = octalToDecimal(stoi(header->size));
+    		if (size == 0) {
+			header++;
+		}
+		else {
+			Elf64_Ehdr *elfhdr = (Elf64_Ehdr *) (header+1);
+			if((elfhdr->e_ident[0]==0x7f)&&(elfhdr->e_ident[1]==0x45)&&
+			(elfhdr->e_ident[2]==0x4c)&&(elfhdr->e_ident[3]==0x46)&& (!strcmp(header->name,fileName))) {
+				Task *new_task = (Task*) kmalloc(sizeof(Task));
+				if (run_queue == NULL) {
+					run_queue = new_task;
+				}
+				else {
+					new_task->next = run_queue;
+					run_queue = new_task;
+				}
+				// TODO: setting CURRENT_TASK here for now but need to set in schedule
+				CURRENT_TASK = new_task;	
+				// TODO: hard coding pid for now, remove later
+				new_task->pid = (last_assn_pid+1)%MAX_PROC;
+				last_assn_pid = new_task->pid;
+				new_task->mm = (struct mm_struct *) kmalloc((sizeof(struct mm_struct)));
+				new_task->mm->vm_begin = NULL;
+				uint8_t *data = (uint8_t *)(header+1);
+				Elf64_Phdr *proghdr = (Elf64_Phdr *)&data[elfhdr->e_phoff];
+				struct vma* iter;
+				int i;
+				uint64_t end_addr = 0;
+				for(i = 0; i < elfhdr->e_phnum; i++) {
+					if(proghdr[i].p_type == ELF_PT_LOAD) {
+						struct vma *vm = (struct vma*) kmalloc(sizeof(struct vma));
+						vm->vma_start = (uint64_t *)proghdr[i].p_vaddr;
+						if((proghdr[i].p_vaddr + proghdr[i].p_memsz) > end_addr) {
+							end_addr = (proghdr[i].p_vaddr + proghdr[i].p_memsz);
+						}
+						vm->vma_end = (uint64_t *)(proghdr[i].p_vaddr + proghdr[i].p_memsz);
+						vm->vma_file_ptr = (uint64_t *)(&data[proghdr[i].p_offset]); 
+						// GSAHA: added to test page fault handle
+						vm->vma_file_offset = 0;
 
-        Task *t = (Task*) kmalloc(sizeof(Task));
-	if (run_queue == NULL) run_queue = t;
-	else {
-	    t->next = run_queue;
-	    run_queue = t;
+						vm->vma_size = proghdr[i].p_filesz;
+						vm->vma_flags = proghdr[i].p_flags;
+						vm->vma_next = NULL;
+						if(new_task->mm->vm_begin == NULL) {
+							new_task->mm->vm_begin = vm;
+						}
+						else {
+							for(iter = new_task->mm->vm_begin; iter->vma_next != NULL; iter = iter->vma_next);
+							iter->vma_next = vm;
+						} 
+					}
+				}
+				end_addr += 4096;
+				end_addr = (end_addr >> 12) << 12;
+				// Go to the end of the Vma list and add vma for heap
+				for(iter = new_task->mm->vm_begin; iter->vma_next != NULL; iter = iter->vma_next);
+				struct vma *vm = (struct vma*) kmalloc(sizeof(struct vma));
+				vm->vma_start = (uint64_t *) end_addr;
+				vm->vma_end = (uint64_t *) end_addr;
+				vm->vma_next = NULL;
+				iter->vma_next = vm;
+				// Add vma entry for stack
+				struct vma *vm_stack = (struct vma*) kmalloc(sizeof(struct vma));
+				vm_stack->vma_start = (uint64_t *) 0xc0000000 - 0x10000000;
+				vm_stack->vma_end = (uint64_t *) 0xc0000000;
+				vm_stack->vma_next = NULL;
+				vm->vma_next = vm_stack;
+				// Allocate 1 page for stack for now and add it to the new cr3 page mapping
+				uint64_t newcr3 = createTable();
+				uint64_t oldcr3;
+				__asm__ __volatile__("movq %%cr3, %0\n\t"
+						    :"=a"(oldcr3));
+				__asm__ __volatile__("movq %0, %%cr3\n\t"
+						    ::"a"(newcr3));
+				new_task->mm->pg_pml4=newcr3;
+				new_task->regs.cr3 = newcr3;
+				put_page_mapping(7,0xc0000000, newcr3);
+				put_page_mapping(7,0xc0000000 - 4096, newcr3);
+				new_task->mm->stack_begin = (uint64_t) (0xc0000000);
+				__asm__ __volatile__("movq %0, %%cr3\n\t"
+						    ::"a"(oldcr3));        
+				new_task->mm->e_entry = elfhdr->e_entry;
+				return new_task;
+			}
+			size = (size%512==0) ? size +512: size + 512 + (512-size%512);
+			header = (struct posix_header_ustar *) (((uint64_t)(header)) + size);
+		}
 	}
-	// TODO: setting CURRENT_TASK here for now but need to set in schedule
-	CURRENT_TASK = t;	
-	// TODO: hard coding pid for now, remove later
-	t->pid = (last_assn_pid+1)%MAX_PROC;
-	last_assn_pid = t->pid;
-        t->mm = (struct mm_struct *) kmalloc((sizeof(struct mm_struct)));
-        t->mm->vm_begin = NULL;
-        uint8_t *data = (uint8_t *)(header+1);
-        Elf64_Phdr *proghdr = (Elf64_Phdr *)&data[elfhdr->e_phoff];
-        struct vma* iter;
-        int i;
-        uint64_t end_addr = 0;
-        for(i=0;i<elfhdr->e_phnum;i++) {
-
-          if(proghdr[i].p_type == ELF_PT_LOAD) {
-            kprintf("need to load this\n");
-            struct vma *vm = (struct vma*) kmalloc(sizeof(struct vma));
-            vm->vma_start = (uint64_t *)proghdr[i].p_vaddr;
-            if((proghdr[i].p_vaddr + proghdr[i].p_memsz) > end_addr) {
-              end_addr = (proghdr[i].p_vaddr + proghdr[i].p_memsz);
-            }
-            vm->vma_end = (uint64_t *)(proghdr[i].p_vaddr + proghdr[i].p_memsz);
-            vm->vma_file_ptr = (uint64_t *)(&data[proghdr[i].p_offset]); 
-	    // GSAHA: added to test page fault handler
-	    vm->vma_file_offset = 0;
-
-            vm->vma_size = proghdr[i].p_filesz;
-            vm->vma_flags = proghdr[i].p_flags;
-            vm->vma_next = NULL;
-            if(t->mm->vm_begin == NULL) t->mm->vm_begin = vm;
-            else {
-             for(iter = t->mm->vm_begin; iter->vma_next != NULL; iter = iter->vma_next);
-             iter->vma_next = vm;
-            } 
-          }
-        }
-        end_addr += 4096;
-        end_addr = (end_addr >> 12) << 12;
-        for(iter = t->mm->vm_begin; iter->vma_next != NULL; iter = iter->vma_next);
-        struct vma *vm = (struct vma*) kmalloc(sizeof(struct vma));
-        vm->vma_start = (uint64_t *) end_addr;
-        vm->vma_end = (uint64_t *) end_addr;
-        vm->vma_next = NULL;
-        iter->vma_next = vm;
-        struct vma *vm_stack = (struct vma*) kmalloc(sizeof(struct vma));
-        vm_stack->vma_start = (uint64_t *) 0xc0000000 - 0x10000000;
-        vm_stack->vma_end = (uint64_t *) 0xc0000000;
-        vm_stack->vma_next = NULL;
-        vm->vma_next = vm_stack;
-        uint64_t newcr3 = createTable();
-        uint64_t oldcr3;
-        __asm__ __volatile__("movq %%cr3, %0\n\t"
-                             :"=a"(oldcr3));
-        __asm__ __volatile__("movq %0, %%cr3\n\t"
-                             ::"a"(newcr3));
-	t->mm->pg_pml4=newcr3;
-        t->regs.cr3 = newcr3;
-        put_page_mapping(7,0xc0000000, newcr3);
-        put_page_mapping(7,0xc0000000 - 4096, newcr3);
-        t->mm->stack_begin = (uint64_t) (0xc0000000);
-        __asm__ __volatile__("movq %0, %%cr3\n\t"
-                             ::"a"(oldcr3));        
-        t->mm->e_entry = elfhdr->e_entry;
-        return t;
-	//switch_user_mode(elfhdr->e_entry, t->regs.rsp);
-	// GSAHA: added to test page fault handler 
-	//switch_user_mode(elfhdr->e_entry);
-
-      }
-      size = (size%512==0) ? size +512: size + 512 + (512-size%512);
-      header = (struct posix_header_ustar *) (((uint64_t)(header)) + size);
-    }  
-  } 
-return NULL; 
+	return NULL; 
 }
