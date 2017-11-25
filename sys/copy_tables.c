db716016f33ef784351311fdd877c2f87d00d5d5
Mayur jadhav <mayurjadhav72@gmail.com>
2017-11-25 06:40:18 -0500

    Page Table Changed 4kb alignment of old pages

--- sys/copy_tables.c
+++ sys/copy_tables.c
@@ -19,18 +19,22 @@ uint64_t copy_on_write() {
 		PML4_new[i] = PML4_old[i];
 		if(PML4_old[i] != 0) {
 			PML4_new[i] = get_physical_free_page();
-			uint64_t *PDTP_old = (uint64_t *)((uint64_t) PML4_old[i] + (uint64_t)0xffffffff80000000 );
-			uint64_t *PDTP_new = (uint64_t *)((uint64_t) PML4_new[i] + (uint64_t)0xffffffff80000000 );
+			uint64_t *PDTP_old = (uint64_t *)((((uint64_t) PML4_old[i] + (uint64_t)0xffffffff80000000) >> 12) <<12 );
+			uint64_t *PDTP_new = (uint64_t *)((uint64_t) PML4_new[i] + (uint64_t)0xffffffff80000000 );	
+			PML4_new[i] |= (uint64_t) 7;
 			for(int j = 0; j < 512; j++) {
 				PDTP_new[j] = PDTP_old[j];
 				if(PDTP_old[j] != 0) {
 					PDTP_new[j] = get_physical_free_page();			
-					uint64_t *PDE_old = (uint64_t *)((uint64_t) PDTP_old[j] + (uint64_t)0xffffffff80000000 );
+					uint64_t *PDE_old = (uint64_t *)((((uint64_t) PDTP_old[j] + (uint64_t)0xffffffff80000000 )>>12) << 12);
 					uint64_t *PDE_new = (uint64_t *)((uint64_t) PDTP_new[j] + (uint64_t)0xffffffff80000000 );
+					PDTP_new[j] |= (uint64_t) 7;
 					for(int k = 0; k < 512; k++) {
 						PDE_new[k] = PDE_old[k];
-						PDE_new[k] = (PDE_new[k] >> 12) << 12;
-						PDE_new[k] |= (uint64_t) 0x805; 
+						if(PDE_old[k] != 0) {
+							PDE_new[k] = (PDE_new[k] >> 12) << 12;
+							PDE_new[k] |= (uint64_t) 0x805; 
+						}
 					}
 				}
 			}
