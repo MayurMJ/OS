24620bb101f1afecc10d8cffd5c7f9e203c63d8a
Mayur jadhav <mayurjadhav72@gmail.com>
2017-12-03 23:19:53 -0500

    WIP malloc

--- include/stdlib.h
+++ include/stdlib.h
@@ -4,6 +4,12 @@
 int main(int argc, char *argv[], char *envp[]);
 void exit(int status);
 
+typedef struct block_header {
+	uint8_t available;
+	uint64_t size;
+	struct block_header *next
+} m_header;
+
 void *malloc(size_t size);
 void free(void *ptr);
 void setenv(char *name, char *value, char *envp[]);
--- libc/stdlib.c
+++ libc/stdlib.c
@@ -7,3 +7,10 @@ void exit(int ret) {
 			   :"=a"(result)
 			   :"a"(syscall_code), "D"(ret));
 }
+
+void *malloc(size_t size) {
+	static m_header start = NULL;
+	static m_header end = NULL;
+	size_t alloc_size = (size + sizeof(m_header) + 15 ) & ~(15);
+	alloc_size = alloc_size << 1;
+}
