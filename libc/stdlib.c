619b1833be2f19ca1569c3ca8d5e9aa26b19a6b5
Mayur jadhav <mayurjadhav72@gmail.com>
2017-12-08 22:49:58 -0500

    execvpe

--- libc/stdlib.c
+++ libc/stdlib.c
@@ -2,6 +2,49 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+int execvpe(const char *file, char *const argv[], char *const envp[]) {
+  execve(file, argv, envp);
+  int i = 0, j = 0, flag = 0, k = 0, l = 0;
+  char envVar[256];
+  char values[50][1024];
+  while(envp[i][0] != '\0') {
+    j = 0;
+    while(envp[i][j] != '='  && envp[i][j] != '\0') {
+      envVar[j] = envp[i][j];
+      j++;
+    }
+    envVar[j] = '\0';
+    j++;
+    if(strcmp(envVar, "PATH") == 0) {
+      flag = 1;
+      break;
+    }
+   i++;
+  }
+  if(flag == 1) {
+    l = 0;
+    while(envp[i][j] != '\0') {
+      while(envp[i][j] != ':' && envp[i][j] != '\0') {
+          values[k][l] = envp[i][j];
+          j++;l++;
+      }
+      values[k][l] = '/'; l++;
+      values[k][l] = '\0';
+      if(envp[i][j] != '\0')
+       { k++;j++;l=0; }
+      else
+        k++;l=0;
+    }
+  }
+  for(i = 0; i < k; i++) {
+    strcat(values[i], file);
+    execve(values[i], argv, envp);
+    i++;
+  }
+  return -1;
+}
 
 void exit(int ret) {
 	uint64_t syscall_code = 60;
